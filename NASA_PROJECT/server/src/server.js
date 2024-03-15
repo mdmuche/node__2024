c7a6e9a33f28e2f89d6e26f28823dae83a7ae075
mdmuche <abulamartins@gmail.com>
2024-03-15 08:38:05 -0700

    added mongoose database

--- NASA_PROJECT/server/src/server.js
+++ NASA_PROJECT/server/src/server.js
@@ -1,4 +1,6 @@
 const http = require("http");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
 
 const app = require("./app");
 
@@ -6,9 +8,20 @@ const { loadPlanetsData } = require("./models/planets.model");
 
 const PORT = process.env.PORT || 8000;
 
+const URL = process.env.MONGO_URL;
+
 const server = http.createServer(app);
 
+mongoose.connection.once("open", () => {
+  console.log("connection to db was successful!");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error(err);
+});
+
 async function startServer() {
+  await mongoose.connect(URL);
   await loadPlanetsData();
   server.listen(PORT, () => {
     console.log(`listening for request at port ${PORT}...`);

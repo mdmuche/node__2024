8004a901ceca86221047ebc25dd87a41d804e98d
mdmuche <abulamartins@gmail.com>
2024-03-23 03:44:28 -0700

    imported and used mongoconnect from services

--- NASA_PROJECT/server/src/server.js
+++ NASA_PROJECT/server/src/server.js
@@ -1,27 +1,16 @@
 const http = require("http");
-const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 
 const app = require("./app");
-
+const { mongoConnect } = require("./services/mongo");
 const { loadPlanetsData } = require("./models/planets.model");
 
 const PORT = process.env.PORT || 8000;
 
-const URL = process.env.MONGO_URL;
-
 const server = http.createServer(app);
 
-mongoose.connection.once("open", () => {
-  console.log("connection to db was successful!");
-});
-
-mongoose.connection.on("error", (err) => {
-  console.error(err);
-});
-
 async function startServer() {
-  await mongoose.connect(URL);
+  await mongoConnect();
   await loadPlanetsData();
   server.listen(PORT, () => {
     console.log(`listening for request at port ${PORT}...`);
